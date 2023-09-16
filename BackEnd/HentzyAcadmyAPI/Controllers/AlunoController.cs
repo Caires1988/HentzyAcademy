c75d4449e59df2a0f64e6ae5dc9dbc85b7799a28
Caires1988 <rodrigokaires1712@gmail.com>
2023-09-16 12:22:44 -0300

    fiz a criacao do endpoint creataluno

---
+++ BackEnd/HentzyAcadmyAPI/Controllers/AlunoController.cs
@@ -0,0 +1,17 @@
+﻿using HentzyAcadmyAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HentzyAcadmyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunoController : ControllerBase
+    {
+
+        [HttpPost(Name= "CreateAluno")]
+        public ActionResult CreateAluno (Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+    }
+}
