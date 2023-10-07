2b3b3a85fa2d8d919c209d5b9a239476d9e69086
diovanibatista <diovanibueno@gmail.com>
2023-10-07 12:24:57 -0300

    commit front

---
+++ FrontEnd/academiaAPP/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { CadastroAlunosComponent } from './components/pages/cadastro-alunos/cadastro-alunos.component';
+import { ConsultaAlunosComponent } from './components/pages/consulta-alunos/consulta-alunos.component';
+import { EdicaoAlunosComponent } from './components/pages/edicao-alunos/edicao-alunos.component';
+import { AppRoutingModule } from './app.routing';
+import { CadastroFuncionariosComponent } from './components/pages/cadastro-funcionarios/cadastro-funcionarios.component';
+import { ConsultaFuncionariosComponent } from './components/pages/consulta-funcionarios/consulta-funcionarios.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CadastroAlunosComponent,
+    ConsultaAlunosComponent,
+    EdicaoAlunosComponent,
+    CadastroFuncionariosComponent,
+    ConsultaFuncionariosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, //Adicionando as configurações das rotas
+    FormsModule, //Para utilizar formulários
+    ReactiveFormsModule //Para utilizar formulários reativos
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontEnd/academiaAPP/src/app/app.routing.ts
@@ -0,0 +1,29 @@
+import { CadastroFuncionariosComponent } from './components/pages/cadastro-funcionarios/cadastro-funcionarios.component';
+import { RouterModule, Routes } from "@angular/router";
+import { CadastroAlunosComponent } from "./components/pages/cadastro-alunos/cadastro-alunos.component";
+import { ConsultaAlunosComponent } from "./components/pages/consulta-alunos/consulta-alunos.component";
+import { EdicaoAlunosComponent } from "./components/pages/edicao-alunos/edicao-alunos.component";
+
+import { NgModule } from "@angular/core";
+import { ConsultaFuncionariosComponent } from './components/pages/consulta-funcionarios/consulta-funcionarios.component';
+
+
+// mapeamento das rotas do projeto
+const routes: Routes = [
+{ path: 'pages/cadastro-alunos', component: CadastroAlunosComponent },
+{ path: 'pages/consulta-alunos', component: ConsultaAlunosComponent },
+{ path: 'pages/edicao-alunos', component: EdicaoAlunosComponent },
+{path: 'pages/cadastro-funcionarios', component: CadastroFuncionariosComponent},
+{path: 'pages/consultar-funcionarios', component: ConsultaFuncionariosComponent},
+{ path: '', pathMatch: 'full', redirectTo:'pages/consulta-alunos' }
+
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+
+export class AppRoutingModule {}
